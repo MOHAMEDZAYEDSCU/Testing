f4b5859247cb25f56fbf6eb1411aee1027f7570c
MOHAMEDZAYEDSCU <118567156+MOHAMEDZAYEDSCU@users.noreply.github.com>
2023-10-11 22:58:04 +0300

    modified

--- contributor.sh
+++ contributor.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-USERNAME="MOHAMEDZAYEDSC"
-REPO_NAME="Testing"
-BRANCH_NAME="master"
-
-cd /home/zayed/Testing
-
 git add .
 git commit -m "Auto-commit at $(date)"
-git push origin $BRANCH_NAME
+git push
 
